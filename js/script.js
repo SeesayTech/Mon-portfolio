db1be8bb7ca8e1f445399752f1de6903098e636a
SeesayTech <seesaytech@gmail.com>
2025-02-13 16:05:03 +0000

    Responsive design version 1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const landing = document.querySelector(".landing");
 const arrow_up = document.querySelector(".hide");
 window.addEventListener("scroll", () => {
-  if (landing.getBoundingClientRect().top <= 60) {
+  if (landing.getBoundingClientRect().top <= -110) {
     arrow_up.classList.add("arrow-up");
     arrow_up.classList.remove("hide");
     console.log(landing.getBoundingClientRect().top);
@@ -9,5 +9,24 @@ window.addEventListener("scroll", () => {
     arrow_up.classList.add("hide");
     arrow_up.classList.remove("arrow-up");
     console.log(landing.getBoundingClientRect().top);
+  }
+});
+
+// menu toggler
+
+const menuToggler = document.getElementById("menu-toggler");
+const menu = document.getElementById("menu");
+let showed = false;
+
+menuToggler.addEventListener("click", () => {
+  if(showed === false){
+    menu.style.transform = "translateX(0vw)";
+    menu.style.transition = "all .6s";
+    showed = true;
+  }
+  else{
+    menu.style.transform = "translateX(100vw)";
+    menu.style.transition = "all .6s";
+    showed = false;
   }
 });
